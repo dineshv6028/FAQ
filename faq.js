dfbeb2656703edc446ad553adfc076e8128f3a1b
dineshv6028 <dineshv6028@gmail.com>
2024-07-25 16:47:46 +0530

    Add files via upload

---
+++ faq.js
@@ -0,0 +1,17 @@
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq)=>{
+    faq.addEventListener("click", ()=>{
+        if(faq.classList.contains("active")){
+            faq.classList.remove("active");
+        }
+        else{
+            faq.classList.add("active");
+            faqs.forEach((otherFaq)=>{
+                if(otherFaq != faq){
+                    otherFaq.classList.remove("active");
+                }
+            })
+        }
+    })
+})
